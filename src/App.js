d887099c3121ae52b3342ac6a54f52954b525f85
Toan <68524814+briantoanle@users.noreply.github.com>
2025-05-31 13:18:14 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { Component, useState, useEffect } from 'react';
 import './App.css';
+import { Auth } from "./components/auth";
 
+import { db } from './config/firebase';
+import { getDocs, collection } from 'firebase/firestore';
 function App() {
+  const [spendingList, setSpendingList] = useState([]);
+  const spendingCollectionRef = collection(db, "Budget");
+
+  useEffect(() => {
+    const getSpendingList = async () => {
+      // Read list from database, set spending list
+      try {
+        const data = await getDocs(spendingCollectionRef);
+        const filteredData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+        console.log(filteredData);
+        setSpendingList(filteredData)
+      } catch (err) {
+        console.error(err);
+      }
+
+    };
+    getSpendingList();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Auth />
+      
+      <div>
+        {spendingList.map((spending) =>(
+          <div>
+          <h1>
+            Spending list:
+            <p>
+            Date: {spending["Date of Purchase"]?.toDate().toLocaleDateString()}
+            Item: {spending["Item Description"]}
+            Price: {spending["Price"]}
+            Store: {spending["Store"]}
+            </p>
+          </h1>
+          </div>
+        ))}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
